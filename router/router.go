b5282f0f8719abfc3d362cd4f750899bbb034752
meguriri <1310500986@qq.com>
2023-01-16 19:44:47 +0800

    change router

--- router/router.go
+++ router/router.go
@@ -1,17 +1,13 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/meguriri/OnlineAssessmentSystem/handler"
 )
 
 func InitRouter(router *gin.Engine) {
-	router.LoadHTMLGlob("static/html/*")
-	router.StaticFS("/css", http.Dir(".static/css"))
-	router.StaticFS("/images", http.Dir(".static/images"))
-	router.StaticFS("/js", http.Dir(".static/js"))
+	router.Static("/static", "./static/")
+	router.LoadHTMLGlob("templates/*")
 	router.POST("/login", handler.Login)
 	router.POST("/register", handler.Register)
 }
